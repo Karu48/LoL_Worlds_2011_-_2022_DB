729cbd352daa77162434bdb510888d82416fd09c
Karu48 <rodrigo.rln.04@gmail.com>
2024-05-18 20:35:27 -0500

    extra tests EH

--- EHBenchmark.cpp
+++ EHBenchmark.cpp
@@ -1,5 +1,31 @@
 #include "ExtendibleHashing/ExtendibleHashing.h"
 
+void test10(){
+    fstream file("data/playerStats.csv", ios::in);
+    string line;
+    getline(file, line);
+    ExtendibleHashing<Register> eh("t10.dat", 6, 3, CustomHash);
+    for (int i = 0; i < 10; i++){
+        getline(file, line);
+        Register r(line, "player", "player");
+        eh.insert(r);
+    }
+    // eh.printAllBucketsFromDir();
+}
+
+void test100(){
+    fstream file("data/playerStats.csv", ios::in);
+    string line;
+    getline(file, line);
+    ExtendibleHashing<Register> eh("t100.dat", 6, 3, CustomHash);
+    for (int i = 0; i < 100; i++){
+        getline(file, line);
+        Register r(line, "player", "player");
+        eh.insert(r);
+    }
+    // eh.printAllBucketsFromDir();
+}
+
 void test1000(){
     fstream file("data/playerStats.csv", ios::in);
     string line;
@@ -14,6 +40,10 @@ void test1000(){
 }
 
 int main(){
+    test10();
+
+    test100();
+    
     test1000();
     return 0;
 }
